05bb16c2070b192f1fc89c575017d49b68c3111b
unknown <1148690152@qq.com>
2019-01-04 16:04:42 +0800

    表单校验功能正式完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,4 +1,6 @@
-import { invalid } from "moment";
+import {
+    invalid
+} from "moment";
 
 $(function () {
 
@@ -27,6 +29,9 @@ $(function () {
                         min: 2,
                         max: 6,
                         message: '用户名长度必须是2-6位'
+                    },
+                    callback: {
+                        message: '用户名不存在'
                     }
                 }
             },
@@ -42,14 +47,70 @@ $(function () {
                         min: 6,
                         max: 12,
                         message: '密码长度必须是2-6位'
+                    },
+                    callback: {
+                        message: '密码错误'
                     }
                 }
             }
+            
         }
 
     })
 
 
-    
+
+    // 2.我们需要用到插件的校验功能,所以要用submit按钮
+    // ,所以需要注册表单校验成功事件,在事件中,组织默认的提交(防止跳转),通过ajax提交即可
+
+    // 注册表单校验成功时间
+
+    $('#form').on('success.form.bv', function (e) {
+
+        // 阻止默认的提交
+        e.preventDefault();
+
+        // 通过ajax提交
+
+        $.ajax({
+            type: "post",
+            url: "/employee/employeeLogin",
+            data: $('#form').serialize(),
+            dataType: "json",
+            success: function (info) {
+                if (info.error === 1000) {
+                    // alert('用户名不存在');
+                    // 调用插件实例方法 更新校验状态成失败,提示用户
+                    $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+                    return;
+                }
+                if (info.error === 1001) {
+                    // alert('密码错误');
+                    $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+
+                    return;
+                }
+                if (info.sussess) {
+                    location.href = 'index.html';
+                    return;
+                }
+            }
+        });
+
+    })
+
+
+    // 3.重置功能.
+    //  默认 type='reset' 按钮, 只会重置表单内容
+    // 此时,内容和校验状态都需要重置,需要调用插件的实例方法
+    // $('#form').data('bootstrapValidator')创建插件实例
+    // resetForm() 不传参  只重置校验状态
+    // resetForm(true) 传true , 内容和状态都重置
+
+    $('[type="reset"]').click(function () {
+
+        $('#form').data('bootstrapValidator').resetForm();
+
+    })
 
 });
